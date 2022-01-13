e01f5de50f809d28ec53110c768709e2cffbedb9
Konrad Grela <konrad1997@gmail.com>
2022-01-13 13:19:35 +0100

    Remove redundant files from the build output

--- build-win32-x64.bat
+++ build-win32-x64.bat
@@ -17,4 +17,4 @@ call del picgal-populate-database.spec
 
 call cd ..
 
-call npx electron-packager . picgal --out=out/ --win32metadata.ProductName="Picgal" --win32metadata.CompanyName="konrad2508" --platform=win32 --arch=x64 --ignore="(server/src*|server/requirements.txt|node_modules|.gitignore|.vscode|build-linux-x64|build-win32-x64.bat|public*|previews*|database.sqlite)"
+call npx electron-packager . picgal --out=out/ --win32metadata.ProductName="Picgal" --win32metadata.CompanyName="konrad2508" --platform=win32 --arch=x64 --ignore="(server/src*|server/requirements.txt|node_modules|.gitignore|.vscode|build-linux-x64|build-win32-x64.bat|public*|previews*|database.sqlite|src/components*|src/enums*|src/forms*|src/hooks*|src/services*|src/styles*|src/App.jsx|src/index.jsx)"
