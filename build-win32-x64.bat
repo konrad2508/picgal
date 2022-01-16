297c28e64af574d3b40e0d329a46b22e0e2c1611
Konrad Grela <33187831+konrad2508@users.noreply.github.com>
2022-01-16 15:32:19 +0100

    Add sync_database.py in favour of populate_database.py

--- build-win32-x64.bat
+++ build-win32-x64.bat
@@ -1,6 +1,9 @@
 @echo off
+call npm i
+call pip install -r server/requirements.txt
+call pip install pyinstaller
 
-call rmdir /s /q out\picgal-win32-x64
+call rmdir /s /q out\Picgal-win32-x64
 
 call rmdir /s /q build
 call npm run build
@@ -11,9 +14,9 @@ call pyinstaller --onefile --name picgal-server src/app.py
 call rmdir /s /q build
 call del picgal-server.spec 
 
-call pyinstaller --onefile --name picgal-populate-database src/populate_database.py
+call pyinstaller --onefile --name picgal-sync-database src/sync_database.py
 call rmdir /s /q build
-call del picgal-populate-database.spec
+call del picgal-sync-database.spec
 
 call pyinstaller --onefile --name picgal-rebuild-previews src/rebuild_previews.py
 call rmdir /s /q build
@@ -21,4 +24,4 @@ call del picgal-rebuild-previews.spec
 
 call cd ..
 
-call npx electron-packager . picgal --out=out/ --win32metadata.ProductName="Picgal" --win32metadata.CompanyName="konrad2508" --platform=win32 --arch=x64 --ignore="(^/server/src*|^/server/requirements.txt|^/node_modules/@.*|^/node_modules/.cache|^/node_modules/.package-lock.json|^/.gitignore|^/.vscode|^/build-linux-x64|^/build-win32-x64.bat|^/public*|^/previews*|^/database.sqlite|^/src/components*|^/src/enums*|^/src/forms*|^/src/hooks*|^/src/services*|^/src/styles*|^/src/App.jsx|^/src/index.jsx)"
+call npx electron-packager . Picgal --out=out/ --win32metadata.ProductName="Picgal" --win32metadata.CompanyName="konrad2508" --platform=win32 --arch=x64 --ignore="(^/server/src*|^/server/requirements.txt|^/node_modules/@.*|^/node_modules/.cache|^/node_modules/.package-lock.json|^/.gitignore|^/.vscode|^/build-linux-x64|^/build-win32-x64.bat|^/public*|^/previews*|^/database.sqlite|^/src/components*|^/src/enums*|^/src/forms*|^/src/hooks*|^/src/services*|^/src/styles*|^/src/App.jsx|^/src/index.jsx)"
