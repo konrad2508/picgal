f4fb360ae8224949550fe1761413670bf8aaeff1
Konrad Grela <33187831+konrad2508@users.noreply.github.com>
2022-01-27 21:45:20 +0100

    Include rebuild_samples.py in build scripts

--- build-win32-x64.bat
+++ build-win32-x64.bat
@@ -22,6 +22,10 @@ call pyinstaller --onefile --name picgal-rebuild-previews src/rebuild_previews.p
 call rmdir /s /q build
 call del picgal-rebuild-previews.spec
 
+call pyinstaller --onefile --name picgal-rebuild-samples src/rebuild_samples.py
+call rmdir /s /q build
+call del picgal-rebuild-samples.spec
+
 call cd ..
 
-call npx electron-packager . Picgal --out=out/ --win32metadata.ProductName="Picgal" --win32metadata.CompanyName="konrad2508" --platform=win32 --arch=x64 --ignore="(^/server/src*|^/server/requirements.txt|^/node_modules/@.*|^/node_modules/.cache|^/node_modules/.package-lock.json|^/.gitignore|^/.vscode|^/build-linux-x64|^/build-win32-x64.bat|^/public*|^/previews*|^/database.sqlite|^/src/components*|^/src/enums*|^/src/forms*|^/src/hooks*|^/src/services*|^/src/styles*|^/src/App.jsx|^/src/index.jsx)"
+call npx electron-packager . Picgal --out=out/ --win32metadata.ProductName="Picgal" --win32metadata.CompanyName="konrad2508" --platform=win32 --arch=x64 --ignore="(^/server/src*|^/server/requirements.txt|^/node_modules/@.*|^/node_modules/.cache|^/node_modules/.package-lock.json|^/.gitignore|^/.vscode|^/build-linux-x64|^/build-win32-x64.bat|^/public*|^/previews*|^/samples*|^/database.sqlite|^/src/components*|^/src/enums*|^/src/forms*|^/src/hooks*|^/src/services*|^/src/styles*|^/src/App.jsx|^/src/index.jsx)"
