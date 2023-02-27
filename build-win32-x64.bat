08f4d5d702ce09974a11b1efc9b2d6bd73032ac9
Konrad Grela <33187831+konrad2508@users.noreply.github.com>
2023-02-27 01:36:32 +0100

    Remove redundant scripts

--- build-win32-x64.bat
+++ build-win32-x64.bat
@@ -14,18 +14,6 @@ call pyinstaller --onefile -i NONE --name picgal-server src/app.py
 call rmdir /s /q build
 call del picgal-server.spec 
 
-call pyinstaller --onefile -i NONE --name picgal-sync-database src/sync_database.py
-call rmdir /s /q build
-call del picgal-sync-database.spec
-
-call pyinstaller --onefile -i NONE --name picgal-rebuild-previews src/rebuild_previews.py
-call rmdir /s /q build
-call del picgal-rebuild-previews.spec
-
-call pyinstaller --onefile -i NONE --name picgal-rebuild-samples src/rebuild_samples.py
-call rmdir /s /q build
-call del picgal-rebuild-samples.spec
-
 call cd ..
 
 call npx electron-packager . Picgal --out=out/ --win32metadata.ProductName="Picgal" --win32metadata.CompanyName="konrad2508" --platform=win32 --arch=x64 --icon=./build/icon.ico --ignore="(^/server/src*|^/server/requirements.txt|^/node_modules/@.*|^/node_modules/.cache|^/node_modules/.package-lock.json|^/.gitignore|^/.vscode|^/build-linux-x64|^/build-win32-x64.bat|^/public*|^/previews*|^/samples*|^/database.sqlite|^/src/components*|^/src/enums*|^/src/forms*|^/src/hooks*|^/src/services*|^/src/styles*|^/src/App.jsx|^/src/index.jsx)"
