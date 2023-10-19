4e4b215f6ff209d6cd314c10ff14e744e8820dcf
Konrad Grela <konrad1997@gmail.com>
2023-10-19 19:08:31 +0200

    Refactor App

--- src/App.jsx
+++ src/App.jsx
@@ -4,83 +4,15 @@ import React from 'react';
 import Title from './components/Title';
 import Menu from './components/Menu';
 import Content from './components/Content';
-import Command from './enums/Command';
 import useAppState from './hooks/useAppState';
 import AppContext from './components/context/AppContext';
 
 const App = () => {
-    const { appState, switchState } = useAppState();
-
-    const sendQuery = (event) => switchState(Command.SEARCH, { event });
-    const onImagePreviewClick = (img) => switchState(Command.PREVIEW, { img });
-    const handleQueryChange = (event) => switchState(Command.QUERY_CHANGE, { event });
-    const onClickTag = (tag) => switchState(Command.CLICK_TAG, { tag });
-    const onBackClick = () => switchState(Command.CLICK_BACK, {  });
-    const onPageNavClick = (pageStep) => switchState(Command.PAGE_NAV, { pageStep });
-    const onSaveModifiedTagsClick = (id, modifications) => switchState(Command.MODIFY_IMG, { id, modifications });
-    const onSearchFavouritesClick = () => switchState(Command.CLICK_FAVOURITES, {  });
-    const onClickSavedQuery = (savedQuery) => switchState(Command.CLICK_SAVED_QUERY, { savedQuery })
-    const onModifySavedQuery = (id, modifications) => switchState(Command.MODIFY_SAVED_QUERY, { id, modifications });
-    const onDeleteSavedQuery = (id) => switchState(Command.DELETE_SAVED_QUERY, { id });
-    const onAddSavedQuery = (newSavedQuery) => switchState(Command.ADD_SAVED_QUERY, { newSavedQuery });
-    const onSyncDatabase = () => switchState(Command.SYNC_DATABASE, {  });
-    const onStartBatchEditor = () => switchState(Command.START_BATCH_EDITOR, {  });
-    const onStartSettings = () => switchState(Command.START_SETTINGS, {  });
-    const onSaveSettings = (modifications) => switchState(Command.SAVE_SETTINGS, { modifications });
-    const onClickPreviewInBatchEditor = (img) => switchState(Command.CLICK_PREVIEW_IN_BATCH_EDITOR, { img });
-    const sendQueryInBatchEditor = (event) => switchState(Command.SEARCH_IN_BATCH_EDITOR, { event });
-    const onCancelBatchEditor = () => switchState(Command.CANCEL_BATCH_EDITOR, {  });
-    const onClickTitle = () => switchState(Command.CLICK_TITLE, {  });
-    const onSearchFavouritesClickInBatchEditor = () => switchState(Command.CLICK_FAVOURITES_IN_BATCH_EDITOR, {  });
-    const onClickSavedQueryInBatchEditor = (savedQuery) => switchState(Command.CLICK_SAVED_QUERY_IN_BATCH_EDITOR, { savedQuery });
-    const onSaveModifiedTagsClickInBatchEditor = (_, modifications) => switchState(Command.MODIFY_IMG_IN_BATCH_EDITOR, { modifications });
-    const onPageNavClickInBatchEditor = (pageStep) => switchState(Command.PAGE_NAV_IN_BATCH_EDITOR, { pageStep });
-
-    const appContextValue = {
-        appState: appState.appState,
-        query: appState.query,
-        usedQuery: appState.usedQuery,
-        savedQueries: appState.savedQueries,
-        imagesToShow: appState.imagesToShow,
-        existingTags: appState.existingTags,
-        pageNumber: appState.currentPage,
-        maxPage: appState.maxPage,
-        deletedCounter: appState.deletedCounter,
-        restoredPreviewsCounter: appState.restoredPreviewsCounter,
-        restoredSamplesCounter: appState.restoredSamplesCounter,
-        addCounter: appState.addCounter,
-        historyLength: appState.history.length - 1,
-        batchEditorSelected: appState.batchEditorImages,
-        config: appState.config,
-        sendQuery,
-        handleQueryChange,
-        onImagePreviewClick,
-        onPageNavClick,
-        onBackClick,
-        onClickTag,
-        onSaveModifiedTagsClick,
-        onSearchFavouritesClick,
-        onClickSavedQuery,
-        onModifySavedQuery,
-        onDeleteSavedQuery,
-        onAddSavedQuery,
-        onSyncDatabase,
-        onStartBatchEditor,
-        onStartSettings,
-        onSaveSettings,
-        onClickPreviewInBatchEditor,
-        sendQueryInBatchEditor,
-        onCancelBatchEditor,
-        onClickTitle,
-        onSearchFavouritesClickInBatchEditor,
-        onClickSavedQueryInBatchEditor,
-        onSaveModifiedTagsClickInBatchEditor,
-        onPageNavClickInBatchEditor
-    };
+    const contextValue = useAppState();
 
     return (
         <div className={styles.main}>
-            <AppContext.Provider value={appContextValue}>
+            <AppContext.Provider value={contextValue}>
                 <Title/>
                 <div className={styles.app}>
                     <Menu/>
--- src/hooks/useAppState.js
+++ src/hooks/useAppState.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import appStateService from '../services/appStateService';
-import Command from '../enums/Command';
 import AppState from '../enums/AppState';
 
 const useAppState = () => {
-    const [query, setQuery] = React.useState('');
-    const [imagesToShow, setImagesToShow] = React.useState([]);
-    const [appState, setAppState] = React.useState(AppState.START);
-    const [usedQuery, setUsedQuery] = React.useState('');
-    const [currentPage, setCurrentPage] = React.useState(1);
-    const [maxPage, setMaxPage] = React.useState(1);
-    const [existingTags, setExistingTags] = React.useState([]);
-    const [savedQueries, setSavedQueries] = React.useState([]);
-    const [deletedCounter, setDeletedCounter] = React.useState(-1);
-    const [restoredPreviewsCounter, setRestoredPreviewsCounter] = React.useState(-1);
-    const [restoredSamplesCounter, setRestoredSamplesCounter] = React.useState(-1);
-    const [addCounter, setAddCounter] = React.useState(-1);
-    const [batchEditorImages, setBatchEditorImages] = React.useState([]);
-    const [config, setConfig] = React.useState({});
+    const [ query, setQuery ] = React.useState('');
+    const [ imagesToShow, setImagesToShow ] = React.useState([]);
+    const [ appState, setAppState ] = React.useState(AppState.START);
+    const [ usedQuery, setUsedQuery ] = React.useState('');
+    const [ currentPage, setCurrentPage ] = React.useState(1);
+    const [ maxPage, setMaxPage ] = React.useState(1);
+    const [ existingTags, setExistingTags ] = React.useState([]);
+    const [ savedQueries, setSavedQueries ] = React.useState([]);
+    const [ deletedCounter, setDeletedCounter ] = React.useState(-1);
+    const [ restoredPreviewsCounter, setRestoredPreviewsCounter ] = React.useState(-1);
+    const [ restoredSamplesCounter, setRestoredSamplesCounter ] = React.useState(-1);
+    const [ addCounter, setAddCounter ] = React.useState(-1);
+    const [ batchEditorImages, setBatchEditorImages ] = React.useState([]);
+    const [ config, setConfig ] = React.useState({});
 
     const clearState = () => {
         setQuery('');
@@ -27,7 +26,7 @@ const useAppState = () => {
         setCurrentPage(1);
         setMaxPage(1);
     };
-    const [history, setHistory] = React.useState([clearState]);
+    const [ history, setHistory] = React.useState([clearState]);
 
     const setters = {
         setQuery,
@@ -50,210 +49,105 @@ const useAppState = () => {
 
     React.useEffect(hookService.fetchSavedDataEffect, []);
 
-    const switchState = (command, args) => {
-        switch (command) {
-            case Command.SEARCH: {
-                const { event } = args;
-                event.preventDefault();
+    const sendQuery = (event) => {
+        event.preventDefault();
 
-                hookService.searchCommand(query);
-
-                break;
-            }
-
-            case Command.PREVIEW: {
-                const { img } = args;
-
-                hookService.previewCommand(img);
-
-                break;
-            }
-
-            case Command.QUERY_CHANGE: {
-                const { event } = args;
-
-                hookService.queryChangeCommand(event);
-
-                break;
-            }
-
-            case Command.CLICK_TAG: {
-                const { tag } = args;
-
-                hookService.clickTagCommand(tag);
-
-                break;
-            }
-
-            case Command.CLICK_BACK: {
-                hookService.clickBackCommand();
-
-                break;
-            }
-
-            case Command.PAGE_NAV: {
-                const { pageStep } = args;
-                
-                hookService.pageNavCommand(usedQuery, currentPage, pageStep);
-
-                break;
-            }
-
-            case Command.MODIFY_IMG: {
-                const { id, modifications } = args;
-
-                hookService.modifyImageCommand(id, modifications);
-                
-                break;
-            }
-
-            case Command.CLICK_FAVOURITES: {
-                hookService.clickFavouritesCommand();
-
-                break;
-            }
-
-            case Command.CLICK_SAVED_QUERY: {
-                const { savedQuery } = args;
-                
-                hookService.clickSavedQueryCommand(savedQuery.query);
-
-                break;
-            }
-
-            case Command.MODIFY_SAVED_QUERY: {
-                const { id, modifications } = args;
-                
-                hookService.modifySavedQueryCommand(id, modifications, savedQueries);
-
-                break;
-            }
-
-            case Command.DELETE_SAVED_QUERY: {
-                const { id } = args;
-
-                hookService.deleteSavedQueryCommand(id, savedQueries);
-
-                break;
-            }
-            
-            case Command.ADD_SAVED_QUERY: {
-                const { newSavedQuery } = args;
-
-                hookService.addSavedQueryCommand(newSavedQuery, savedQueries);
-
-                break;
-            }
-
-            case Command.SYNC_DATABASE: {
-                hookService.syncDatabase();
-
-                break;
-            }
-
-            case Command.START_BATCH_EDITOR: {
-                hookService.startBatchEditorCommand();
-                
-                break;
-            }
-            
-            case Command.CLICK_PREVIEW_IN_BATCH_EDITOR: {
-                const { img } = args;
-
-                hookService.clickPreviewInBatchEditorCommand(img);
-                
-                break;
-            }
-
-            case Command.SEARCH_IN_BATCH_EDITOR: {
-                const { event } = args;
-                event.preventDefault();
-
-                hookService.searchInBatchEditorCommand(query);
-
-                break;
-            }
-
-            case Command.CANCEL_BATCH_EDITOR: {
-                hookService.cancelBatchEditorCommand();
-
-                break;
-            }
-
-            case Command.START_SETTINGS: {
-                hookService.startSettingsCommand();
-
-                break;
-            }
-
-            case Command.SAVE_SETTINGS: {
-                const { modifications } = args;
-
-                hookService.saveSettingsCommand(modifications);
-
-                break;
-            }
-
-            case Command.CLICK_TITLE: {
-                hookService.clickTitleCommand();
-
-                break;
-            }
-
-            case Command.CLICK_FAVOURITES_IN_BATCH_EDITOR: {
-                hookService.clickFavouritesInBatchEditorCommand();
-
-                break;
-            }
-
-            case Command.CLICK_SAVED_QUERY_IN_BATCH_EDITOR: {
-                const { savedQuery } = args;
-                
-                hookService.clickSavedQueryInBatchEditorCommand(savedQuery.query);
-
-                break;
-            }
-
-            case Command.MODIFY_IMG_IN_BATCH_EDITOR: {
-                const { modifications } = args;
-
-                hookService.modifyImageInBatchEditorCommand(modifications);
-                
-                break;
-            }
-
-            case Command.PAGE_NAV_IN_BATCH_EDITOR: {
-                const { pageStep } = args;
-                
-                hookService.pageNavInBatchEditorCommand(usedQuery, currentPage, pageStep);
-
-                break;
-            }
-
-            default: { }
-        }        
+        hookService.searchCommand(query);
     };
 
-    return {
-        appState: {
-            query,
-            imagesToShow,
-            appState,
-            usedQuery,
-            currentPage,
-            maxPage,
-            history,
-            existingTags,
-            savedQueries,
-            deletedCounter,
-            restoredPreviewsCounter,
-            restoredSamplesCounter,
-            addCounter,
-            batchEditorImages,
-            config
-        },
-        switchState
+    const onImagePreviewClick = (img) => hookService.previewCommand(img);
+
+    const handleQueryChange = (event) => hookService.queryChangeCommand(event);
+
+    const onClickTag = (tag) => hookService.clickTagCommand(tag);
+
+    const onBackClick = () => hookService.clickBackCommand();
+
+    const onPageNavClick = (pageStep) => hookService.pageNavCommand(usedQuery, currentPage, pageStep);
+
+    const onSaveModifiedTagsClick = (id, modifications) => hookService.modifyImageCommand(id, modifications);
+
+    const onSearchFavouritesClick = () => hookService.clickFavouritesCommand();
+
+    const onClickSavedQuery = (savedQuery) => hookService.clickSavedQueryCommand(savedQuery.query);
+
+    const onModifySavedQuery = (id, modifications) => hookService.modifySavedQueryCommand(id, modifications, savedQueries);
+
+    const onDeleteSavedQuery = (id) => hookService.deleteSavedQueryCommand(id, savedQueries);
+
+    const onAddSavedQuery = (newSavedQuery) => hookService.addSavedQueryCommand(newSavedQuery, savedQueries);
+
+    const onSyncDatabase = () => hookService.syncDatabase();
+
+    const onStartBatchEditor = () => hookService.startBatchEditorCommand();
+
+    const onStartSettings = () => hookService.startSettingsCommand();
+
+    const onSaveSettings = (modifications) => hookService.saveSettingsCommand(modifications);
+
+    const onClickPreviewInBatchEditor = (img) => hookService.clickPreviewInBatchEditorCommand(img);
+
+    const sendQueryInBatchEditor = (event) => {
+        event.preventDefault();
+
+        hookService.searchInBatchEditorCommand(query);
+    }
+
+    const onCancelBatchEditor = () => hookService.cancelBatchEditorCommand();
+
+    const onClickTitle = () => hookService.clickTitleCommand();
+
+    const onSearchFavouritesClickInBatchEditor = () => hookService.clickFavouritesInBatchEditorCommand();
+
+    const onClickSavedQueryInBatchEditor = (savedQuery) => hookService.clickSavedQueryInBatchEditorCommand(savedQuery.query);
+
+    const onSaveModifiedTagsClickInBatchEditor = (_, modifications) => hookService.modifyImageInBatchEditorCommand(modifications);
+
+    const onPageNavClickInBatchEditor = (pageStep) => hookService.pageNavInBatchEditorCommand(usedQuery, currentPage, pageStep);
+
+    const contextValue = {
+        appState,
+        query,
+        usedQuery,
+        savedQueries,
+        imagesToShow,
+        existingTags,
+        pageNumber: currentPage,
+        maxPage,
+        deletedCounter,
+        restoredPreviewsCounter,
+        restoredSamplesCounter,
+        addCounter,
+        historyLength: history.length - 1,
+        batchEditorSelected: batchEditorImages,
+        config,
+        sendQuery,
+        handleQueryChange,
+        onImagePreviewClick,
+        onPageNavClick,
+        onBackClick,
+        onClickTag,
+        onSaveModifiedTagsClick,
+        onSearchFavouritesClick,
+        onClickSavedQuery,
+        onModifySavedQuery,
+        onDeleteSavedQuery,
+        onAddSavedQuery,
+        onSyncDatabase,
+        onStartBatchEditor,
+        onStartSettings,
+        onSaveSettings,
+        onClickPreviewInBatchEditor,
+        sendQueryInBatchEditor,
+        onCancelBatchEditor,
+        onClickTitle,
+        onSearchFavouritesClickInBatchEditor,
+        onClickSavedQueryInBatchEditor,
+        onSaveModifiedTagsClickInBatchEditor,
+        onPageNavClickInBatchEditor
     };
+
+    return contextValue;
 };
 
 export default useAppState;
