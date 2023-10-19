0c666723326b8d83a41e1653f8ce25fa1dcf0269
Konrad Grela <konrad1997@gmail.com>
2023-10-19 22:09:45 +0200

    Refactor ModifiableSavedQueriesList

--- src/components/ModifiableSavedQueriesList.jsx
+++ src/components/ModifiableSavedQueriesList.jsx
@@ -5,60 +5,24 @@ import ModifiableSavedQuery from './ModifiableSavedQuery';
 import AddNewSavedQuery from './AddNewSavedQuery';
 import StubFavourites from './StubFavourites';
 import useModifiableSavedQueriesListState from '../hooks/useModifiableSavedQueriesListState';
-import ModifiableSavedQueriesListCommand from '../enums/ModifiableSavedQueriesListCommand';
-import AppContext from './context/AppContext';
-import SavedQueriesListContext from './context/SavedQueriesListContext';
 import ModifiableSavedQueriesListContext from './context/ModifiableSavedQueriesListContext';
 
-const ModifiableSavedQueriesList = () => {               
-    const { exitModificationMode } = React.useContext(SavedQueriesListContext);
-    const { savedQueries, onAddSavedQuery } = React.useContext(AppContext);
-
-    const { modifiableSavedQueriesListState, setModifiableSavedQueriesListState } = useModifiableSavedQueriesListState();
-    
-    const startAddingSavedQuery = () => setModifiableSavedQueriesListState(ModifiableSavedQueriesListCommand.START_ADDING_SAVED_QUERY, { });
-    const handleInputNewName = (event) => setModifiableSavedQueriesListState(ModifiableSavedQueriesListCommand.HANDLE_INPUT_NEW_NAME, { event });
-    const handleInputNewQuery = (event) => setModifiableSavedQueriesListState(ModifiableSavedQueriesListCommand.HANDLE_INPUT_NEW_QUERY, { event });
-    const addNewSavedQuery = () =>
-        setModifiableSavedQueriesListState(ModifiableSavedQueriesListCommand.ADD_SAVED_QUERY, { canUseSavedQueryName, onAddSavedQuery });
-    const cancelAddingSavedQuery = () => setModifiableSavedQueriesListState(ModifiableSavedQueriesListCommand.CANCEL_ADDING_SAVED_QUERY, { });
-    
-    const canUseSavedQueryName = (id, name) => {
-        if (name === '') {
-            return false;
-        }
-
-        const potentialDuplicate = savedQueries.find((e) => e.name === name && e.id !== id);
-        if (potentialDuplicate) {
-            return false;
-        }
-
-        return true;
-    };
-
-    const modifiableSavedQueriesListContextValue = {
-        inputNewName: modifiableSavedQueriesListState.inputNewName,
-        inputNewQuery: modifiableSavedQueriesListState.inputNewQuery,
-        handleInputNewName,
-        handleInputNewQuery,
-        addNewSavedQuery,
-        cancelAddingSavedQuery,
-        canUseSavedQueryName
-    };
+const ModifiableSavedQueriesList = () => {
+    const { usedContextValue, contextValue } = useModifiableSavedQueriesListState();
 
     return (
-        <ModifiableSavedQueriesListContext.Provider value={modifiableSavedQueriesListContextValue}>
+        <ModifiableSavedQueriesListContext.Provider value={contextValue}>
             <div className={styles.titleBoxContainer}>
                 <h3>Saved queries</h3>
                 <div className={styles.titleBoxButtonContainer}>
                     <button
                         className={styles.titleBoxButton}
-                        onClick={startAddingSavedQuery}
-                        disabled={modifiableSavedQueriesListState.addingSavedQuery}
+                        onClick={contextValue.startAddingSavedQuery}
+                        disabled={contextValue.addingSavedQuery}
                     >
                         <FaPlus className='fontAwesome'/>
                     </button>
-                    <button className={styles.titleBoxButton} onClick={exitModificationMode}>
+                    <button className={styles.titleBoxButton} onClick={usedContextValue.exitModificationMode}>
                         <FaTimes className='fontAwesome'/>
                     </button>
                 </div>
@@ -66,10 +30,10 @@ const ModifiableSavedQueriesList = () => {
 
             <div className={styles.savedQueriesOuterContainer}>
                 <div className={styles.savedQueriesInnerContainer}>
-                    { modifiableSavedQueriesListState.addingSavedQuery && <AddNewSavedQuery/> }
+                    { contextValue.addingSavedQuery && <AddNewSavedQuery/> }
 
                     <StubFavourites/>
-                    { savedQueries.map((e) => <ModifiableSavedQuery key={e.id} savedQuery={e}/>)}
+                    { usedContextValue.savedQueries.map((e) => <ModifiableSavedQuery key={e.id} savedQuery={e}/>)}
                 </div>
             </div>
         </ModifiableSavedQueriesListContext.Provider>
--- src/enums/ModifiableSavedQueriesListCommand.js
+++
@@ -1,9 +0,0 @@
-const ModifiableSavedQueriesListCommand = {
-    START_ADDING_SAVED_QUERY: 'Start_Adding_Saved_Query',
-    HANDLE_INPUT_NEW_NAME: 'Handle_Input_New_Name',
-    HANDLE_INPUT_NEW_QUERY: 'Handle_Input_New_Query',
-    ADD_SAVED_QUERY: 'Add_Saved_Query',
-    CANCEL_ADDING_SAVED_QUERY: 'Cancel_Adding_Saved_Query'
-};
-
-export default ModifiableSavedQueriesListCommand;
