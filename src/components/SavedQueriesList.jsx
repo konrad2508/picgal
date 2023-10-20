8d8a54ca23fab768ea819d4873a6ae31d47ffba4
Konrad Grela <33187831+konrad2508@users.noreply.github.com>
2023-10-20 14:01:47 +0200

    Refactor SavedQueriesList

--- src/components/SavedQueriesList.jsx
+++ src/components/SavedQueriesList.jsx
@@ -4,43 +4,15 @@ import { FaAngleRight, FaAngleDown, FaPen } from 'react-icons/fa';
 import SavedQuery from './SavedQuery';
 import ModifiableSavedQueriesList from './ModifiableSavedQueriesList';
 import useSavedQueriesListState from '../hooks/useSavedQueriesListState';
-import SavedQueriesListCommand from '../enums/SavedQueriesListCommand';
-import AppContext from './context/AppContext';
 import SavedQueriesListContext from './context/SavedQueriesListContext';
 import AppState from '../enums/AppState';
 
 const SavedQueriesList = () => {
-    const {
-        onSearchFavouritesClick,
-        onSearchFavouritesClickInBatchEditor,
-        savedQueries,
-        onClickSavedQuery,
-        onClickSavedQueryInBatchEditor,
-        appState
-    } = React.useContext(AppContext);
-
-    const { savedQueriesListState, setSavedQueriesListState } = useSavedQueriesListState();
-    
-    const executeSavedQuery = (savedQuery) =>
-        setSavedQueriesListState(SavedQueriesListCommand.EXECUTE_SAVED_QUERY, { savedQuery, onClickSavedQuery });
-    const executeSearchFavourites = () => setSavedQueriesListState(SavedQueriesListCommand.EXECUTE_SEARCH_FAVOURITES, { onSearchFavouritesClick });
-    const toggleDisplaySavedQueries = () => setSavedQueriesListState(SavedQueriesListCommand.TOGGLE_DISPLAY_SAVED_QUERIES, { });
-    const enterModificationMode = () => setSavedQueriesListState(SavedQueriesListCommand.ENTER_MODIFICATION_MODE, { });
-    const exitModificationMode = () => setSavedQueriesListState(SavedQueriesListCommand.EXIT_MODIFICATION_MODE, { });
-    const executeSearchFavouritesInBatchEditor = () =>
-        setSavedQueriesListState(SavedQueriesListCommand.EXECUTE_SEARCH_FAVOURITES, { onSearchFavouritesClick: onSearchFavouritesClickInBatchEditor });
-    const executeSavedQueryInBatchEditor = (savedQuery) =>
-        setSavedQueriesListState(SavedQueriesListCommand.EXECUTE_SAVED_QUERY, { savedQuery, onClickSavedQuery: onClickSavedQueryInBatchEditor });
-
-    const savedQueriesListContextValue = {
-        executeSavedQuery,
-        executeSearchFavourites,
-        exitModificationMode
-    };
+    const { usedContextValue, contextValue } = useSavedQueriesListState();
 
     const renderModifyButton = () => {
         return (
-            <button className={styles.titleBoxButton} onClick={enterModificationMode}>
+            <button className={styles.titleBoxButton} onClick={contextValue.enterModificationMode}>
                 <FaPen className='fontAwesome'/>
             </button>
         );
@@ -52,14 +24,22 @@ const SavedQueriesList = () => {
                 <div className={styles.savedQueriesInnerContainer}>
                     <SavedQuery
                         savedQueryName={'Favourites'}
-                        onClickSavedQuery={appState === AppState.BATCH_EDITING ? executeSearchFavouritesInBatchEditor : executeSearchFavourites}
+                        onClickSavedQuery={
+                            usedContextValue.appState === AppState.BATCH_EDITING
+                                ? contextValue.executeSearchFavouritesInBatchEditor
+                                : contextValue.executeSearchFavourites
+                        }
                     />
                     {
-                        savedQueries.map((e) =>
+                        usedContextValue.savedQueries.map((e) =>
                             <SavedQuery 
                                 key={e.id}
                                 savedQueryName={e.name}
-                                onClickSavedQuery={() => appState === AppState.BATCH_EDITING ? executeSavedQueryInBatchEditor(e) : executeSavedQuery(e)}
+                                onClickSavedQuery={
+                                    () => usedContextValue.appState === AppState.BATCH_EDITING
+                                        ? contextValue.executeSavedQueryInBatchEditor(e)
+                                        : contextValue.executeSavedQuery(e)
+                                }
                             />
                         )
                     }
@@ -69,7 +49,7 @@ const SavedQueriesList = () => {
     };
 
     const renderSavedQueriesList = () => {
-        if (savedQueriesListState.modificationMode) {
+        if (contextValue.modificationMode) {
             return (
                 <ModifiableSavedQueriesList/>
             );
@@ -80,22 +60,22 @@ const SavedQueriesList = () => {
                     <div className={styles.titleBoxContainer}>
                         <h3>Saved queries</h3>
                         <div className={styles.titleBoxButtonContainer}>
-                            { savedQueriesListState.displaySavedQueries && renderModifyButton() }
-                            <button className={styles.titleBoxButton} onClick={toggleDisplaySavedQueries}>
-                                { savedQueriesListState.displaySavedQueries
+                            { contextValue.displaySavedQueries && renderModifyButton() }
+                            <button className={styles.titleBoxButton} onClick={contextValue.toggleDisplaySavedQueries}>
+                                { contextValue.displaySavedQueries
                                     ? <FaAngleDown className='fontAwesome'/>
                                     : <FaAngleRight className='fontAwesome'/> }
                             </button>
                         </div>
                     </div>
-                    { savedQueriesListState.displaySavedQueries && renderSavedQueries() }
+                    { contextValue.displaySavedQueries && renderSavedQueries() }
                 </>
             );
         }
     };
 
     return (
-        <SavedQueriesListContext.Provider value={savedQueriesListContextValue}> 
+        <SavedQueriesListContext.Provider value={contextValue}> 
             { renderSavedQueriesList() }
         </SavedQueriesListContext.Provider>
     )
