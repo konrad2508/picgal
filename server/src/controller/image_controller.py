917c0a352834b2aeb9efa0bb27e530e13b7624ac
Konrad Grela <33187831+konrad2508@users.noreply.github.com>
2023-11-14 23:42:04 +0100

    Make controllers use services instead of factories

--- server/src/controller/image_controller.py
+++ server/src/controller/image_controller.py
@@ -1,17 +1,17 @@
 import flask
 
 from controller.i_controller import IController
-from factory.i_controller_service_factory import IControllerServiceFactory
 from model.exception.database_integrity_violated import DatabaseIntegrityViolated
 from model.exception.entity_not_found import EntityNotFound
 from model.image.enum.view_encrypted import ViewEncrypted
 from model.image.request.image_modification_request import ImageModificationRequest
 from model.image.request.encrypt_request import EncryptRequest
+from service.image.i_image_controller_service import IImageControllerService
 
 
 class ImageController(IController):
-    def __init__(self, factory: IControllerServiceFactory, route_prefix: str) -> None:
-        self.factory = factory
+    def __init__(self, image_service: IImageControllerService, route_prefix: str) -> None:
+        self.image_service = image_service
         self.route_prefix = route_prefix
 
     def initialize(self) -> flask.Blueprint:
@@ -21,8 +21,6 @@ class ImageController(IController):
         preview_route = f'{self.route_prefix}/preview'
         sample_route = f'{self.route_prefix}/sample'
 
-        image_service = self.factory.get_image_service()
-
         image_controller = flask.Blueprint('image', __name__)
 
         @image_controller.route(f'{info_route}', methods=['GET'])
@@ -31,7 +29,7 @@ class ImageController(IController):
             page = flask.request.args.get('page', type=int, default=1)
             view_encrypted = flask.request.args.get('viewEncrypted', type=ViewEncrypted, default=ViewEncrypted.NO)
 
-            info = image_service.get_infos(
+            info = self.image_service.get_infos(
                 image_url=image_route,
                 preview_url=preview_route,
                 sample_url=sample_route,
@@ -47,7 +45,7 @@ class ImageController(IController):
             batch_modifications = ImageModificationRequest.from_json(flask.request.get_json(force=True))
 
             try:
-                modified_images = image_service.modify_info_batch(image_route, preview_route, sample_route, batch_modifications)
+                modified_images = self.image_service.modify_info_batch(image_route, preview_route, sample_route, batch_modifications)
 
                 return flask.jsonify(modified_images)
 
@@ -62,7 +60,7 @@ class ImageController(IController):
             modifications = ImageModificationRequest.from_json(flask.request.get_json(force=True))
 
             try:
-                modified_image = image_service.modify_info(image_route, preview_route, sample_route, id, modifications)
+                modified_image = self.image_service.modify_info(image_route, preview_route, sample_route, id, modifications)
 
                 return flask.jsonify(modified_image)
             
@@ -77,7 +75,7 @@ class ImageController(IController):
             tags = flask.request.args.get('tags')
             view_encrypted = flask.request.args.get('viewEncrypted', type=ViewEncrypted, default=ViewEncrypted.NO)
 
-            count = image_service.get_infos_count(tags, view_encrypted)
+            count = self.image_service.get_infos_count(tags, view_encrypted)
 
             return flask.jsonify(count)
 
@@ -85,14 +83,14 @@ class ImageController(IController):
         def get_tags() -> flask.Response:
             view_encrypted = flask.request.args.get('viewEncrypted', type=ViewEncrypted, default=ViewEncrypted.NO)
             
-            tags = image_service.get_tags(view_encrypted)
+            tags = self.image_service.get_tags(view_encrypted)
 
             return flask.jsonify(tags)
 
         @image_controller.route(f'{image_route}/<id>', methods=['GET'])
         def get_image(id: int) -> flask.Response:
             try:
-                image = image_service.get_image_content(id)
+                image = self.image_service.get_image_content(id)
 
                 return flask.send_file(image, mimetype='image/jpeg')
             
@@ -104,7 +102,7 @@ class ImageController(IController):
             images_to_encode = EncryptRequest.from_json(flask.request.get_json(force=True))
 
             try:
-                encoding_result = image_service.toggle_encrypt_files(images_to_encode)
+                encoding_result = self.image_service.toggle_encrypt_files(images_to_encode)
 
                 return flask.jsonify(encoding_result)
             
@@ -115,7 +113,7 @@ class ImageController(IController):
         @image_controller.route(f'{preview_route}/<id>', methods=['GET'])
         def get_preview(id: int) -> flask.Response:
             try:
-                preview = image_service.get_preview_content(id)
+                preview = self.image_service.get_preview_content(id)
 
                 return flask.send_file(preview, mimetype='image/jpeg')
             
@@ -125,7 +123,7 @@ class ImageController(IController):
         @image_controller.route(f'{sample_route}/<id>', methods=['GET'])
         def get_sample(id: int) -> flask.Response:
             try:
-                sample = image_service.get_sample_content(id)
+                sample = self.image_service.get_sample_content(id)
 
                 return flask.send_file(sample, mimetype='image/jpeg')
             
--- server/src/controller/rpc_controller.py
+++ server/src/controller/rpc_controller.py
@@ -1,31 +1,29 @@
 import flask
 
 from controller.i_controller import IController
-from factory.i_controller_service_factory import IControllerServiceFactory
 from model.rpc.request.config_request import ConfigRequest
+from service.rpc.i_rpc_controller_service import IRPCControllerService
 
 
 class RpcController(IController):
-    def __init__(self, factory: IControllerServiceFactory, route_prefix: str) -> None:
-        self.factory = factory
+    def __init__(self, rpc_service: IRPCControllerService, route_prefix: str) -> None:
+        self.rpc_service = rpc_service
         self.route_prefix = route_prefix
 
     def initialize(self) -> flask.Blueprint:
         rpc_route = f'{self.route_prefix}/rpc'
 
-        rpc_service = self.factory.get_rpc_service()
-
         rpc_controller = flask.Blueprint('rpc', __name__)
 
         @rpc_controller.route(f'{rpc_route}/sync', methods=['POST'])
         def sync_database() -> flask.Response:
-            syncing_result = rpc_service.sync_database()
+            syncing_result = self.rpc_service.sync_database()
 
             return flask.jsonify(syncing_result)
 
         @rpc_controller.route(f'{rpc_route}/config', methods=['GET'])
         def get_config() -> flask.Response:
-            config = rpc_service.get_config()
+            config = self.rpc_service.get_config()
 
             return flask.jsonify(config)
 
@@ -34,7 +32,7 @@ class RpcController(IController):
             modifications = ConfigRequest.from_json(flask.request.get_json(force=True))
 
             try:
-                modified_config = rpc_service.modify_config(modifications)
+                modified_config = self.rpc_service.modify_config(modifications)
 
                 return flask.jsonify(modified_config)
 
