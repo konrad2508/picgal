a019f11aeb6df7bd3cfb2e0da6d5877b5c18ba48
Konrad Grela <33187831+konrad2508@users.noreply.github.com>
2024-09-06 01:45:53 +0200

    Add new file and orientation virtual tags

--- backend/src/datasource/virtual_tags.py
+++ backend/src/datasource/virtual_tags.py
@@ -7,15 +7,22 @@ def generate_virtual_tags() -> list[VirtualTag]:
     tags = [
         (VirtualTag('orientation')
             .add_subtag('portrait', SubtagCondition(lambda: Image.height > Image.width))
-            .add_subtag('landscape', SubtagCondition(lambda: Image.height < Image.width))),
+            .add_subtag('landscape', SubtagCondition(lambda: Image.height < Image.width))
+            .add_subtag('square', SubtagCondition(lambda: Image.height == Image.width))),
 
         (VirtualTag('favourite')
             .add_subtag('yes', SubtagCondition(lambda: Image.favourite == True))
             .add_subtag('no', SubtagCondition(lambda: Image.favourite == False))),
-        
+
         (VirtualTag('encrypted')
             .add_subtag('yes', SubtagCondition(lambda: Image.encrypted == True))
-            .add_subtag('no', SubtagCondition(lambda: Image.encrypted == False)))
+            .add_subtag('no', SubtagCondition(lambda: Image.encrypted == False))),
+
+        (VirtualTag('file')
+            .add_subtag('png', SubtagCondition(lambda: Image.file.endswith('.png')))
+            .add_subtag('jpg', SubtagCondition(lambda: Image.file.endswith('.jpg') | Image.file.endswith('.jpeg')))
+            .add_subtag('webp', SubtagCondition(lambda: Image.file.endswith('.webp')))
+            .add_subtag('gif', SubtagCondition(lambda: Image.file.endswith('.gif')))),
     ]
 
     return tags
