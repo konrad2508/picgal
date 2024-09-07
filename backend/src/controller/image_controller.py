a02d2c00cee0a82b55ae1751ed227d590e3c7a17
Konrad Grela <33187831+konrad2508@users.noreply.github.com>
2024-09-07 23:21:45 +0200

    Disable caching for images, previews and samples

--- backend/src/controller/image_controller.py
+++ backend/src/controller/image_controller.py
@@ -93,7 +93,10 @@ class ImageController(IController):
             try:
                 image = self.image_service.get_image_content(id)
 
-                return flask.send_file(image, mimetype='image/jpeg')
+                response = flask.make_response(flask.send_file(image, mimetype='image/jpeg'))
+                response.headers['Cache-Control'] = 'no-store'
+
+                return response
             
             except (EntityNotFound,  FileNotFoundError):
                 flask.abort(404)
@@ -129,7 +132,10 @@ class ImageController(IController):
             try:
                 preview = self.image_service.get_preview_content(id)
 
-                return flask.send_file(preview, mimetype='image/jpeg')
+                response = flask.make_response(flask.send_file(preview, mimetype='image/jpeg'))
+                response.headers['Cache-Control'] = 'no-store'
+
+                return response
             
             except (EntityNotFound, FileNotFoundError):
                 flask.abort(404)
@@ -139,7 +145,10 @@ class ImageController(IController):
             try:
                 sample = self.image_service.get_sample_content(id)
 
-                return flask.send_file(sample, mimetype='image/jpeg')
+                response = flask.make_response(flask.send_file(sample, mimetype='image/jpeg'))
+                response.headers['Cache-Control'] = 'no-store'
+
+                return response
             
             except (EntityNotFound, FileNotFoundError):
                 flask.abort(404)
